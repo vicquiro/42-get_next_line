8cb99c7ac20c533002333e274b6fb7a5fb7f5391
Victor Quiroga Reyes <vquiroga@c3r6s5.42madrid.com>
2022-10-15 20:43:30 +0200

    proofs

--- prueba.c
+++ prueba.c
@@ -6,41 +6,60 @@
 /*   By: vquiroga <vquiroga@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 16:44:05 by vquiroga          #+#    #+#             */
-/*   Updated: 2022/10/15 16:50:16 by vquiroga         ###   ########.fr       */
+/*   Updated: 2022/10/15 18:26:18 by vquiroga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void	ft_read_line(int fd, char **keep, char **tmp)
+// void	ft_read_line(int fd, char **keep, char **tmp)
+// {
+// 	char	*buf;
+// 	int		r;
+
+// 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+// 	if (!buf)
+// 		return ;
+// 	r = 1;
+// 	while (r > 0)
+// 	{
+// 		r = read(fd, buf, BUFFER_SIZE);
+// 		// if (r == -1)
+// 		// {
+// 		// 	ft_free_strs(&buf, keep, tmp);
+// 		// 	return ;
+// 		// }
+// 		buf[r] = '\0';
+		
+// 			break ;
+// 	}
+// }
+
+char	*get_next_line(int fd)
 {
 	char	*buf;
 	int		r;
 
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
-		return ;
+		return 0;
 	r = 1;
-	while (r > 0)
-	{
+	// while (r > 0)
+	// {
 		r = read(fd, buf, BUFFER_SIZE);
-		if (r == -1)
-		{
-			ft_free_strs(&buf, keep, tmp);
-			return ;
-		}
-		buf[r] = '\0';
-		*tmp = ft_strdup(*keep);
-		ft_free_strs(keep, 0, 0);
-		*keep = join_strs(*tmp, buf);
-		ft_free_strs(tmp, 0, 0);
-		if (contains_newline(*keep))
-			break ;
-
-
-char	*get_next_line(int fd)
-{
+		// if (r == -1)
+		// {
+		// 	ft_free_strs(&buf, keep, tmp);
+		// 	return ;
+		// }
+		//buf[r] = '\0';
+		
+		//break ;
+	// }
+	return buf;
 }
 
 int	main(int argc, char **argv)
@@ -49,7 +68,6 @@ int	main(int argc, char **argv)
 	char	*line;
 
 	(void)argc;
-
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (-1);
